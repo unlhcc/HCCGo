966d59d9f96ffdd802efb783bea1648905303ebb
Adam Fitzgibbon <afitzgibbon2@unl.edu>
2016-04-01 16:27:24 -0500

    Old jobs can now be deleted from history.

--- src/js/jobHistoryCtrl.js
+++ src/js/jobHistoryCtrl.js
@@ -55,9 +55,41 @@ jobHistoryModule.service('jobService', function() {
 
   // load json file
   var filePath = filePathService.getFilePath();
+  var jsonFile
   $.getJSON(filePath, function(json) {
     $scope.jobs = json.jobs;
+    jsonFile = json;
   });
+
+  $scope.deleteJob = function(job) {
+    bootbox.confirm({
+      message: "Are you sure you want to delete this job?",
+      callback: function(result) {
+        if(result) {
+          // remove panel
+          $("#panel"+job.id).fadeOut(500, function() {
+            $(this).css({"visibility":"hidden",display:'block'}).slideUp();
+          });
+          // remove from angular binding and reset ids
+          $scope.jobs.splice(job.id,1);
+          for (var i = 0; i < $scope.jobs.length; i++) {
+            $scope.jobs[i].id = i;
+          }
+          // remove entry from json file
+          var fs = require("fs");
+          jsonFile.jobs = $scope.jobs;
+          fs.writeFile(filePath, JSON.stringify(jsonFile, null, 2), function(err) {
+            if(err) {
+              return console.error(err);
+            }
+            else {
+              console.log("Job successfully deleted.");
+            }
+          });
+        }
+      }
+    });
+  }
 
 
 }]);
