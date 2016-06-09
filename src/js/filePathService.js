22f6f4899c207e83e065a285bfdac1500a6fdb89
Jerrod Dixon <robogen@users.noreply.github.com>
2016-06-09 14:42:21 -0500

    Commit to merge upstream repo

---
+++ src/js/filePathService.js
@@ -0,0 +1,17 @@
+filePathService = angular.module('filePathService', []);
+filePathService.service('filePathService', function() {
+
+  var dataPath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/' : '/var/local');
+  var path = require('path');
+  dataPath = path.join(dataPath, 'HCCGo');
+  var filePath = path.join(dataPath, 'jobHistory.json');
+  return {
+    getFilePath: function() {
+      return filePath;
+    },
+    getDataPath: function() {
+      return dataPath;
+    }
+  };
+
+});
