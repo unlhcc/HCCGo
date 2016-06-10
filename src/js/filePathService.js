01b3ba276e245197fcfa6d0f4ef2a9fbc7469336
Jerrod Dixon <robogen@users.noreply.github.com>
2016-06-10 10:06:30 -0500

    Fix filePathService on linux

--- src/js/filePathService.js
+++ src/js/filePathService.js
@@ -1,7 +1,7 @@
 filePathService = angular.module('filePathService', []);
 filePathService.service('filePathService', function() {
 
-  var dataPath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/' : '/var/local');
+  var dataPath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/' : process.env.HOME);
   var path = require('path');
   dataPath = path.join(dataPath, 'HCCGo');
   var filePath = path.join(dataPath, 'jobHistory.json');
