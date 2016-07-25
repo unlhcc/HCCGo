648d9f49c11631b3aca956a1e7a4147f3d1e5c3f
Jerrod Dixon <jtdixon2015@gmail.com>
2016-07-25 17:05:43 -0500

    Persistant navbar view

--- HCCGo/app/js/navbar.js
+++ HCCGo/app/js/navbar.js
@@ -5,6 +5,7 @@ navBar.controller('NavCtrl', ['$route', '$scope', '$routeParams', '$location', '
    // This controller intended purely to manage navigation bar
    // No code beyond navigational controls should be used here
    $scope.params = $routeParams;
+   $scope.currentPath = $location.path();
    var clusterInterface = null;
   
    $scope.logout = function() {
@@ -21,21 +22,24 @@ navBar.controller('NavCtrl', ['$route', '$scope', '$routeParams', '$location', '
    };
 
    // Sets username in nav bar
-   connectionService.getUsername().then(function(username) {
+   /*connectionService.getUsername().then(function(username) {
       $scope.username = username;
-   })
+   })*/
    
    // Nav to jobHistory
    $scope.jobHistory = function() {
       $location.path("cluster/" + $scope.params.clusterId + "/jobHistory");
    }
 
-   /* For Reference
-   $scope.onViewLoad = function viewLoad() {
+   // For Reference
+   $scope.$on('$viewContentLoaded', function() {
       $log.debug("ngView has changed");
-      connectionService.getUsername().then(function(username) {
-         $scope.username = username;
-      }) 
-   }*/
+	  $scope.currentPath = $location.path();
+	  if($scope.username == null){
+         connectionService.getUsername().then(function(username) {
+            $scope.username = username;
+         }) 
+	  }
+   })
    
 }]);
