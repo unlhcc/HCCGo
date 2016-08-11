890c230334321e5519b8280f2b76deb6ac6bf815
Jerrod Dixon <jtdixon2015@gmail.com>
2016-08-11 11:18:03 -0500

    Established notifier class

--- HCCGo/app/js/NotifierService.js
+++ HCCGo/app/js/NotifierService.js
@@ -1,143 +1,104 @@
 
 notifierModule = angular.module('NotifierModule', [])
 
-notifierModule.factory('notifierService',['$log', '$q', '$routeParams', function($log, $q, $routeParams) {
+notifierModule.factory('notifierService',['$log', '$q', '$routeParams', 'toastr', 
+                                           function($log, $q, $routeParams, toastr) {
   
-   const {ipcRenderer} = require('electron');  
+   const {ipcRenderer} = require('electron');
+   const notifier = require('node-notifier');
    var path = require('path');
    var fs = require('fs');
-
+   
+   var setDebug = false;     // Setting to true enables debug messages
+   
+   const toastrOptions = {closeButton: true,
+                          timeOut: 5000,
+				          extendedTimeOut: 5000,
+				          progressBar: true};
+   
    /**
-   * To initiate ssh connections to remote clusters.
+   * To notify user of application dependent on window focus
    *
    */
-      
-  ipcRenderer.on('focus-check-message', (event, arg) => {
-      $log.debug("Window is focused: " + arg);
-  });
-  ipcRenderer.send('focus-check-reply', 'ping');
    
-  // Check the writability of a file
-  var checkWritable = function(file) {
-    
+  // Get window focus status
+  var getWinFocus = function() {
     var deferred = $q.defer();
-    if (!file) {
-      deferred.resolve(false);
-    }
-    
-    // Write to the file to test
-    async.waterfall([
-      // Get the sftp module
-      function(callback) {
-        var sftp_return = connectionList[getClusterContext()].sftp(function (err, sftp) {
-          logger.log("Got sftp now");
-          if (err){
-            return callback(err);
-          }
-          return callback(null, sftp);
-        });
-        
-        if (!sftp_return) {
-          callback("Unable to get sftp handle");
-          logger.log("Unable to get sftp handle");
-          return callback("Unable to get sftp handle");
-        }
-        
-      }, function(sftp, callback){
-        
-        // Check for writeable directory
-        path = require('path');
-        // Try to write to a test file
-        var dirname_path = path.dirname(file);
-        var test_path = path.join(dirname_path, ".hccgo-test" + makeid());
-        
-        sftp.open(test_path, 'w', function(err, handle) {
-          if (err){
-            sftp.end();
-            return callback(err);
-          }
-          sftp.close(handle, function(err) {
-            if (err) {
-              sftp.end();
-              return callback(err);
-            }
-            return callback(null, sftp, test_path);
-          });
-        });
-      },
-      // Now, delete the file
-      function(sftp, test_path, callback) {
-        
-        sftp.unlink(test_path, function(err) {
-          if (err) {
-            sftp.end();
-            return callback(test_path + ": " + err);
-          }
-          sftp.end();
-          return callback(null);
-          
-          
-        });
-        
-      }
-    ], function(err, results) {
-      // Now, the end results
-      if (err) {
-        deferred.reject(err);
-      } else {
-        deferred.resolve(true);
-      }
-
-    });
-
-    return deferred.promise;
+	  ipcRenderer.on('focus-check-message', (event, arg) => {
+          $log.debug("Window is focused: " + arg);
+		  deferred.resolve(arg);
+      });
+      ipcRenderer.send('focus-check-reply', 'ping');
+	return deferred.promise;
+  }  
+   
+  // Pop a success message
+  var success = function(msg, title) {
+    getWinFocus().then(function (response) {
+      if (response) {
+	    toastr.success(msg, title, toastrOptions);
+	  } else {
+	    notifier.notify({
+	      title: title,
+		  message: msg,
+		  icon: void 0,
+		  sound: false,
+		  wait: false
+	    }, function (err, response) {
+	      if (err) {
+		    $log.debug(err);
+		  }
+	    })
+	  }
+	});
   }
 
-   var runCommandQueue = async.queue(function (task, callback) {
-      // Starts Command session
-      connectionList[getClusterContext()].exec(task.name, function(err, stream) {
-         
-         cumulData = "";
-         
-         if (err) {
-           $log.error("Error running command " + task.name + ": "+ err);
-           callback(err, cumulData);
-           return;
-         }
-         
-         stream.on('data', function(data) {
-           
-           $log.debug("Got data: " + data);
-           cumulData += data;
-           
-         }).on('close', function(code, signal) {
-           
-           $log.debug('Stream :: close :: code: ' + code + ', signal: ' + signal);
-           callback(null, cumulData);   // Once the command actually completes full data stored here
-           
-         });
-      });
-   }, 1);
- 
-   var runCommand = function(command) {
+  // Pop a warning message
+  var warning = function(msg) {
+    getWinFocus().then(function (response) {
+      if (response) {
+	    toastr.warning(msg, toastrOptions);
+	  } else {
+	    notifier.notify({
+	      title: 'Warning!',
+		  message: msg,
+		  icon: void 0,
+		  sound: false,
+		  wait: false
+	    }, function (err, response) {
+	      if (err) {
+		    $log.debug(err);
+		  }
+	    })
+	  }
+	});
+  }
 
-      var deferred = $q.defer();         // Used to return promise data
-      
-      runCommandQueue.push({name: command}, function(err, cumulData) {
-          if (err) {
-              deferred.reject("Error running command " + command + ": " + err);
-          } else {
-              deferred.resolve(cumulData);
-          }
-      });
-
-      return deferred.promise;   // Asynchronous command, doesn't really return anything until deferred.resolve is called
-   }
+  // Pop a error message
+  var error = function(msg, title) {
+    getWinFocus().then(function (response) {
+      if (response) {
+	    toastr.error(msg, title, toastrOptions);
+	  } else {
+	    notifier.notify({
+	      title: title,
+		  message: msg,
+		  icon: void 0,
+		  sound: false,
+		  wait: false
+	    }, function (err, response) {
+	      if (err) {
+		    $log.debug(err);
+		  }
+	    })
+	  }
+	});
+  }
   
-   return {
-   getConnection: getConnection,
-   runCommand: runCommand,
-   getUsername: getUsername
-   }
+  return {
+  success: success,
+  warning: warning,
+  error: error
+  }
   
 }]);
