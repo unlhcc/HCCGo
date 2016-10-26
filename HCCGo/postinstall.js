fd13a7b7dcff6e78a920ebeba0b87cd6675315e6
Jerrod Dixon <jtdixon2015@gmail.com>
2016-10-26 13:16:18 -0500

    Staging changes, untested on anything but win32

--- HCCGo/postinstall.js
+++ HCCGo/postinstall.js
@@ -1,12 +1,24 @@
 #!/usr/bin/env node
 var exec = require('child_process').exec;
 
-exec('./node_modules/.bin/electron-rebuild -w ffi',
+/*exec('npm rebiuld --runtime=electron --target=1.4.3 --disturl=https://atom.io/download/atom-shell',
     function(error,stdout,stderr){
-    
+    console.log(error);
+	console.log(stdout);
+	console.log(stderr);
+    });
+*/
+
+exec(__dirname + '\\node_modules\\.bin\\electron-rebuild.cmd -w ffi',
+    function(error,stdout,stderr){
+    console.log(error);
+	console.log(stdout);
+	console.log(stderr);
     });
 
-exec('./node_modules/.bin/electron-rebuild -w ref',
+exec(__dirname + '\\node_modules\\.bin\\electron-rebuild.cmd -w ref',
     function(error,stdout,stderr){
-    
+    console.log(error);
+	console.log(stdout);
+	console.log(stderr);
     });
