596e20e223ce6be9db496e64aa1c0aa173c66f6b
Jerrod Dixon <robogen@users.noreply.github.com>
2016-11-02 13:57:04 -0500

    Requested changes fix

--- HCCGo/postinstall.js
+++ HCCGo/postinstall.js
@@ -2,9 +2,9 @@
 const spawn = require('child_process').spawnSync;
 const fs = require('fs');
 const target = "1.4.3";
+const path = require('path');
 
 var rebuilder = "";
-var brace = "";
 
 if (process.platform == 'win32') {
     rebuilder = "electron-rebuild.cmd";
@@ -16,26 +16,23 @@ if (process.platform == 'win32') {
 
 // nslog fixer
 try {
-    fs.unlinkSync(__dirname + brace + 'node_modules'
-                  + brace + 'nslog' + brace + 'build' + brace 
-                  + 'Release' + brace + 'nslog.node');
+    fs.unlinkSync(path.join(__dirname, 'node_modules', 'nslog', 'build',
+                            'Release', 'nslog.node'));
 } catch (e) {
     console.log("nslog.node doesn't exist\n");
 }
 
-const ref = spawn(__dirname + brace + 'node_modules' + brace + '.bin' 
-                  + brace + rebuilder,
+const ref = spawn(path.join(__dirname, 'node_modules', '.bin', rebuilder),
                   ['--version='+target,'--log'],
-		  { cwd: __dirname + brace + 'node_modules' + brace + 'ref',
+		  { cwd: path.join(__dirname ,'node_modules' ,'ref'),
 		    env: process.env,
 			shell: true });
 console.log('ref output: ' + ref.output);
 console.log('ref error: ' + ref.error);
 
-const ffi = spawn(__dirname + brace + 'node_modules' + brace + '.bin' 
-                  + brace +  rebuilder,
+const ffi = spawn(path.join(__dirname, 'node_modules', '.bin', rebuilder),
                   ['--version='+target,'--log'],
-		  { cwd: __dirname + brace + 'node_modules' + brace + 'ffi',
+		  { cwd: path.join(__dirname, 'node_modules', 'ffi'),
 		    env: process.env,
 			shell: true });
 console.log('ffi output: ' + ffi.output);
