bc3307f25c385745b85df3d98404995264e0d15c
Derek Weitzel <djw8605@gmail.com>
2016-11-30 19:07:25 -0600

    Adding roles

--- HCCGo/main.js
+++ HCCGo/main.js
@@ -45,13 +45,13 @@ app.on('ready', function() {
         ]}, {
         label: "Edit",
         submenu: [
-            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:", role: 'undo' },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:", role: 'redo' },
             { type: "separator" },
-            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:", role: "cut" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:", role: "copy" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:", role: "paste" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:", role: "selectall" }
         ]}
     ];
 
