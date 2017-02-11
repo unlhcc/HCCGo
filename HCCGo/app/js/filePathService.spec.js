207654a90c3a8393b6b2320bba2f445529b695d8
Derek Weitzel <djw8605@gmail.com>
2017-02-10 20:24:20 -0600

    Adding karma

---
+++ HCCGo/app/js/filePathService.spec.js
@@ -0,0 +1,48 @@
+
+describe('File Path Service', function() {
+  var filePathService;
+  process = {}
+  process.env = {}
+  process.platform = 'darwin';
+  process.env.HOME = "/home/derek"
+  path = {}
+  path.join = function() {
+    new_array = [];
+    for (var i = 0; i < arguments.length; i++) {
+      new_array.push(arguments[i]);
+    }
+    return new_array.join('/');
+    
+  }
+  require = function(package) {
+    switch(package) {
+      case "path":
+        return path;
+        break;
+    }
+  }
+  
+  // Before each test load our api.users module
+  beforeEach(angular.mock.module('filePathService'));
+  
+  // Before each test set our injected Users factory (_Users_) to our local Users variable
+  beforeEach(inject(function(_filePathService_) {
+    filePathService = _filePathService_;
+  }));
+  
+  // A simple test to verify the Users factory exists
+  it('should exist', function() {
+    expect(filePathService).toBeDefined();
+  });
+  
+  it('Return job history', function() {
+    expect(filePathService.getJobHistory()).toBe(path.join(process.env.HOME, 'Library/', 'HCCGo', 'jobHistory.db'));
+  });
+  
+  it('Return Data Path', function() {
+    expect(filePathService.getDataPath()).toBe(path.join(process.env.HOME, 'Library/', 'HCCGo'));
+  });
+  
+  
+  
+});
