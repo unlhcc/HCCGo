24ac72679533a23e93b6145697d3db5cd6630720
Jerrod Dixon <jtdixon2015@gmail.com>
2017-02-13 18:07:07 -0600

    Navbar fix to prevent reiteration of username get

--- HCCGo/app/js/navbar.js
+++ HCCGo/app/js/navbar.js
@@ -1,44 +1,9 @@
 navBar = angular.module('HccGoApp.NavCtrl', ['ngRoute' ]);
 
-navBar.controller('NavCtrl', ['$route', '$scope', '$routeParams', '$location', '$log', 'preferencesManager', 'connectionService', 'updaterService', '$rootScope', 
-   function($route,$scope,$routeParams,$location,$log,preferencesManager,connectionService, updaterService, $rootScope) {
+navBar.controller('NavCtrl', ['$scope', '$log', 'navService', 'connectionService', 'updaterService', 
+   function($scope,$log,navService,connectionService, updaterService) {
    // This controller intended purely to manage navigation bar
    // No code beyond navigational controls should be used here
-   $scope.params = $routeParams;
-   $scope.currentPath = $location.path();
-   var clusterInterface = null;
-
-   $scope.logout = function() {
-      connectionService.closeStream();
-      $location.path("/");
-   };
-
-   $scope.goHome = function() {
-     $location.path("/cluster/" + $routeParams.clusterId);
-   };
-
-   $scope.goToSCP = function() {
-      $location.path("/cluster/" + $routeParams.clusterId + "/filesystem");
-   };
-
-   // Sets username in nav bar
-   //$scope.username = $templateCache.get('username');
-   
-   // Nav to jobHistory
-   $scope.jobHistory = function() {
-      $location.path("cluster/" + $scope.params.clusterId + "/jobHistory");
-   }
-   
-   
-
-   // For Reference
-   $log.debug("Username: " + $scope.username);
-   //if($templateCache.get('username') == null){
-      connectionService.getUsername().then(function(username) {
-          $scope.username = username;
-      }) 
-   //} else {
-   //   $scope.username = $templateCache.get('username');
-   //}
+   $scope.servicer = navService; 
    
 }]);
