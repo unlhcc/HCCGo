46a1bc33fe7902b03e4192bfeb04272a7a4ab1bc
cjkeilig <cjkeilig@gmail.com>
2017-02-16 11:44:34 -0600

    use angular

--- HCCGo/app/js/clusterFileSystemCtrl.js
+++ HCCGo/app/js/clusterFileSystemCtrl.js
@@ -7,6 +7,7 @@ clusterUploadModule.controller('clusterFileSystemCtrl', ['$scope', '$log', '$tim
    // Initialization functions
    $scope.params = $routeParams; 
    $scope.fileManage = fileManageService;
+   $scope.focusFile = {};
    if (fileManageService.localFocus != "") {
       angular.element("#l" + fileManageService.localFocus.replace(/\./g, "\\.")).addClass('highlight');
    } else if (fileManageService.remoteFocus != "") {
@@ -19,5 +20,8 @@ clusterUploadModule.controller('clusterFileSystemCtrl', ['$scope', '$log', '$tim
                  function(newValue, oldValue) { 
 				     $log.debug("The files have changed");
 			    });*/
+   $scope.findHighlighted = function(file, place) {
 
+     $scope.focusFile = fileManageService[place].find(function(x) {return file === x.name});
+   }
 }]);
