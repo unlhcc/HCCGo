427b7f4c41fc5479afe61120a7534736c196dbc1
caseylargent <caseylargent@unomaha.edu>
2017-03-06 16:18:54 -0600

    Working on test for DataUsageService

---
+++ test/specs/dataUsageService.spec.js
@@ -0,0 +1,31 @@
+describe('Data Usage Service', () => {
+    var dataUsageService;
+    var $q;
+    var deferred;
+
+    beforeEach(angular.mock.module('Data Usage Service'));
+
+    beforeEach(inject(function(_dataUsageService_) {
+        dataUsageService = dataUsageService;
+    }));
+
+    
+    beforeEach(inject(function(_$q_) {
+        $q = _$q_;
+
+        deferred = $q.defer();
+
+        spyOn(clusterInterface, 'getStorageInfo').and.returnValue(deferred.promise);
+    }));
+
+
+
+    it('should exist', function() {
+        expect(dataUsageService).toBeDefined();
+    });
+
+    it('should be resolved', function() {
+        expect(dataUsageservice.getDataUsage()).not.toBe(undefined);
+
+    });
+});
