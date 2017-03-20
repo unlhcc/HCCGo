a21836706085a500c63f1d07b2e0dadc404ca208
Derek Weitzel <djw8605@gmail.com>
2017-03-20 10:20:11 -0500

    Adding source of user's bashrc, if it exists

--- HCCGo/app/js/ConnectionService.js
+++ HCCGo/app/js/ConnectionService.js
@@ -260,7 +260,8 @@ connectionModule.factory('connectionService',['$log', '$q', '$routeParams', '$lo
       // Starts Command session
       // Always source /etc/bashrc on the remote cluster before
       // running commands
-      real_task = "if [ -f /etc/bashrc ]; then . /etc/bashrc; fi; " + task.name;
+      // How are we going to handle those people whose default shell is not bash?
+      real_task = "if [ -f /etc/bashrc ]; then . /etc/bashrc; fi; if [ -f ~/.bashrc ]; then . ~/.bashrc; fi; " + task.name;
       connectionList[getClusterContext()].exec(real_task, function(err, stream) {
 
          cumulData = "";
