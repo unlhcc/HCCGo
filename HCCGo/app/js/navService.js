066b361a8caeb6abcea2bdb9082e1557eb25df66
caseylargent <caseylargent@unomaha.edu>
2017-04-05 15:54:26 -0500

    Fixed title to use routing title

--- HCCGo/app/js/navService.js
+++ HCCGo/app/js/navService.js
@@ -17,33 +17,27 @@ fileManageService.factory('navService',['$log', '$q', '$location', '$routeParams
    //
    service.currentPath = $location.path();
    service.username = new String("");
-   angular.element(document).find("title").text("Home");
 
    service.logout = function() {
       connectionService.closeStream();
       $location.path("/");
-      angular.element(document).find("title").text("HCC Go");
    };
 
    service.goHome = function() {
      $location.path("/cluster");
-     angular.element(document).find("title").text("Home");
    };
 
    service.goToSCP = function() {
       $location.path("/filesystem");
-      angular.element(document).find("title").text("File Transfer");
    };
    
    // Nav to jobHistory
    service.jobHistory = function() {
       $location.path("/jobHistory");
-      angular.element(document).find("title").text("Job History");
    }
    
    service.goToTutorials = function() {
       $location.path("/tutorials");
-      angular.element(document).find("title").text("Tutorials");
    }
    
    //if($templateCache.get('username') == null){
