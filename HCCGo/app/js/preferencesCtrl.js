a668b6fba713969b09480b5d230edf7c69773513
Adam Fitzgibbon <afitzgibbon2@unl.edu>
2017-04-07 15:37:19 -0500

    Added selectize

--- HCCGo/app/js/preferencesCtrl.js
+++ HCCGo/app/js/preferencesCtrl.js
@@ -16,5 +16,34 @@ preferencesModule.controller('preferencesCtrl', ['$scope', '$log', 'preferencesM
   preferencesManager.getPreferences().then(function(data) {
     $scope.uuid = data.uuid;
   });
-  
+
+  var $selector = $('#clusterSelect').selectize({
+
+    createOnBlur: true,
+    labelField: 'label',
+    searchField: 'label',
+    valueField: 'url',
+    selectOnTab: true,
+    render: {
+      'option': function(data, escape) {
+            return '<div><span class="clusterLabel">' + escape(data.label) + '</span>' + '<span class="url">' + escape(data.url) + '</span></div>';
+        },
+       'option_create': function(data, escape) {
+         return '<div class="create">Hostname: <strong>' +  escape(data.input) + '</strong>&hellip;</div>';
+       }
+     },
+     create: function(input, callback) {
+       new_object = {}
+       new_object.label = input.split(".")[0];
+       new_object.url = input;
+       new_object.type = 'slurm';
+       $scope.clusters.push(new_object);
+       callback(new_object);
+
+     }
+
+  });
+
+  var selection = $selector[0].selectize;
+
 }]);
