0e461fd5ca107da4d19a5f6b9343a6fb8de49200
Derek Weitzel <djw8605@gmail.com>
2017-04-10 14:12:45 -0500

    Adding active detection to sidenav

--- HCCGo/app/js/navbar.js
+++ HCCGo/app/js/navbar.js
@@ -1,8 +1,13 @@
 navBar = angular.module('HccGoApp.NavCtrl', ['ngRoute' ]);
 
-navBar.controller('NavCtrl', ['$scope', '$log', 'navService', 'connectionService', 'updaterService', 
-   function($scope,$log,navService,connectionService, updaterService) {
+navBar.controller('NavCtrl', ['$scope', '$log', 'navService', 'connectionService', 'updaterService', '$location', 
+   function($scope,$log,navService,connectionService, updaterService, $location) {
    // This controller intended purely to manage navigation bar
    // No code beyond navigational controls should be used here
    $scope.servicer = navService;
+   
+   $scope.isActive = function (path) {
+      return ($location.path().substr(0, path.length) === path);
+   }
+   
 }]);
